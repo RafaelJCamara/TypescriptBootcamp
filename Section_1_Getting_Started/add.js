54ac25a3aae8f4497d63bfa4ba055007bc5b1bb1
Rafael Camara <rafaelcamarac@gmail.com>
2023-01-07 22:14:54 +0100

    First TS chapter notes and code and basic project outline

---
+++ Section_1_Getting_Started/add.js
@@ -0,0 +1,5 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+//console.log(add("1", "2")); this will throw a compilation error, because we are sending chars when the inputs should be of type number
+console.log(add(1, 2));
---
+++ Section_1_Getting_Started/add.ts
@@ -0,0 +1,6 @@
+function add(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+//console.log(add("1", "2")); this will throw a compilation error, because we are sending chars when the inputs should be of type number
+console.log(add(1, 2));
