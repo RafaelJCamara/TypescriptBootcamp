a2e1e0ab100422d9896b9794193fdaade9399a04
Rafael Camara <rafaelcamarac@gmail.com>
2023-01-08 18:48:20 +0100

    Add object type code and notes.

--- Section_2_TypeScriptBasics_Basic_Types/app.ts
+++ Section_2_TypeScriptBasics_Basic_Types/app.ts
@@ -10,3 +10,18 @@ console.log(compareStrings("abc", "abc"));
 hasSomething(true);
 
 //console.log(compareStrings("abc", 1));
+
+/* const person : object = {
+  name: "Rafael",
+  age: 26,
+};
+
+
+accessing the age property will give an error because we are defining person as being of type object generic
+console.log(person.age) */
+
+//we can either specify the key type pair or let typescript make the inference for us
+const person: { name: string; age: number } = {
+  name: "Rafael",
+  age: 26,
+};
