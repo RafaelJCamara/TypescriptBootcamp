d257aa01f8f88670bc5c5327f31395904b1e98f9
Rafael Camara <rafaelcamarac@gmail.com>
2023-01-13 22:29:45 +0100

    add more class code

--- Section_5_classes_and_interfaces/app.js
+++ Section_5_classes_and_interfaces/app.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Department = /** @class */ (function () {
     function Department(name, employees) {
         this.name = name;
@@ -17,3 +32,29 @@ dev.addEmployee("PersonA");
 dev.addEmployee("PersonB");
 dev.addEmployee("PersonC");
 dev.listEmployees();
+var ITDepartment = /** @class */ (function (_super) {
+    __extends(ITDepartment, _super);
+    function ITDepartment() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        //you can explicitly call the Department contructor by using the super(constructor_arguments) inside this class's constructor
+        _this.recentEmail = "admin@admin.com";
+        return _this;
+    }
+    Object.defineProperty(ITDepartment.prototype, "mostRecentEmail", {
+        get: function () {
+            return this.recentEmail;
+        },
+        set: function (recent) {
+            this.recentEmail = recent;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    //this will override the method definition defined in the base class
+    ITDepartment.prototype.addEmployee = function (employee) { };
+    return ITDepartment;
+}(Department));
+var itDep = new ITDepartment("IT", []);
+console.log(itDep.mostRecentEmail);
+itDep.mostRecentEmail = "rafael@email.com";
+console.log(itDep.mostRecentEmail);
--- Section_5_classes_and_interfaces/app.ts
+++ Section_5_classes_and_interfaces/app.ts
@@ -19,3 +19,31 @@ dev.addEmployee("PersonB");
 dev.addEmployee("PersonC");
 
 dev.listEmployees();
+
+class ITDepartment extends Department {
+  //you can explicitly call the Department contructor by using the super(constructor_arguments) inside this class's constructor
+  private recentEmail: string = "admin@admin.com";
+
+  //defines the getter of the recentEMail property
+  get mostRecentEmail(): string {
+    return this.recentEmail;
+  }
+
+  //defines the setter of the recentEmail property
+  set mostRecentEmail(recent: string) {
+    this.recentEmail = recent;
+  }
+
+  //this will override the method definition defined in the base class
+  addEmployee(employee: string): void {}
+}
+
+let itDep = new ITDepartment("IT", []);
+// we deal with getters and setters as if they are properties from the class
+console.log(itDep.mostRecentEmail);
+itDep.mostRecentEmail = "rafael@email.com";
+console.log(itDep.mostRecentEmail);
+
+abstract class absClass {
+  abstract someAbsMethod(name: string): void;
+}
